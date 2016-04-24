f48f00e83e8ecaf309805fc99765f97312301900
washwash <washingweb@hotmail.com>
2016-04-24 13:37:08 +0800

    fix password-login

--- src/base-mixins/password-login.js
+++ src/base-mixins/password-login.js
@@ -10,24 +10,18 @@ import {onLogin} from "../common/login-method";
 *   Public methods
 */
 
-export function createUser ({username, email, password}) {
+export function createUser ({email, password}) {
     const options = {
         password,
-        user: {
-            username,
-            email
-        }
+        email
     };
     return this.call("createUser", options).then(onLogin.bind(this));
 }
 
-export function loginWithPassword ({username, email, password}) {
+export function loginWithPassword ({email, password}) {
     const loginParameters = {
         password,
-        user: {
-            username,
-            email
-        }
+        email
     };
     return this.call("login", loginParameters).then(onLogin.bind(this));
 }
